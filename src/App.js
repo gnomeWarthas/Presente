c07cd00cd8cf3a865349796d31d08f2b7a75c0b1
Thomas Wagner <gnomewarthas@gmail.com>
2019-12-12 23:35:44 +0100

    Colors adjustement and navbar animations

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      logoUrl: "./logo.png",
       sections:[
         {
           id:'about',
           number:1,
-          title:'Thomas WAGNER',
+          title:'About me',
           content:"Hello, I am Thomas. I am learning web and application development."
         },
         {
           id:'work',
           number:2,
           title:'Work',
-          content:''
+          content:<Work text="coucouille"/>
         },
         {
           id:'contact',
@@ -27,6 +26,7 @@ class App extends React.Component {
           content:''
         } 
       ],
+      logoUrl: "./logo.png",
       displayedSection:[1,'about']
     }
     this.handleNavClick = this.handleNavClick.bind(this)
@@ -36,12 +36,13 @@ class App extends React.Component {
       if (parseInt(e.target.id[3])===x.number){
         if (x.number !== this.state.displayedSection[0]){
           // menu animation
-          document.getElementsByClassName('currentNav').item(0).classList.remove('currentNav')
+          const previousNavSelected = document.getElementsByClassName('currentNav').item(0)
+          animateCSS('.currentNav', 'navFadeOut', ()=>previousNavSelected.classList.remove('currentNav'))
+          animateCSS('#'+e.target.id, 'navFadeIn')
           e.target.classList.add('currentNav')
           // hide the previous displayed section
-          const previousSection = this.state.displayedSection[1]
           const [outDirection, inDirection] = (this.state.displayedSection[0] < parseInt(e.target.id[3]))?['fadeOutLeft','fadeInRight']:['fadeOutRight','fadeInLeft']
-          animateCSS('#'+previousSection, outDirection, ()=>this.setState({displayedSection:[x.number, x.id]}))
+          animateCSS('#'+this.state.displayedSection[1], outDirection, ()=>this.setState({displayedSection:[x.number, x.id]}))
           // display the clicked section
           animateCSS('#'+x.id, inDirection)
         }
@@ -86,6 +87,13 @@ const Logo = (props) => {
   return (
     <div id="logo">
       <img src={props.img} id="logo" alt="" />
+    </div>
+  )
+}
+const Work = (props) => {
+  return (
+    <div id="work">
+      {props.text}
     </div>
   )
 }
